10bc4b8348d3878a32e402097bda13381ad019d3
Anshika Dwivedi <anshikadwivedi1423@gmail.com>
2026-09-15 08:48:32 +0530

    Create sum and store.c

---
+++ sum and store.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[10][10], sum[10];
+    int rows, cols;
+    int i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter the matrix elements:\n");
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++)
+    {
+        sum[i] = 0;
+
+        for (j = 0; j < cols; j++)
+        {
+            sum[i] = sum[i] + matrix[i][j];
+        }
+    }
+
+    printf("Sum of each row:\n");
+
+    for (i = 0; i < rows; i++)
+    {
+        printf("Row %d = %d\n", i + 1, sum[i]);
+    }
+
+    return 0;
+}
